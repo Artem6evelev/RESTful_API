a1341d27687eb10b40b09668ebcb6c8b0ca17338
Artem Shevelev <74210822+Artem6evelev@users.noreply.github.com>
2021-05-22 11:06:37 +0300

    Add files via upload

---
+++ models/subscriber.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const subscriberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  subscribedToChannel: {
+    type: String,
+    required: true
+  },
+  subscribeDate: {
+    type: Date,
+    required: true,
+    default: Date.now
+  }
+})
+
+// module take 2 properties 1.name of the model in db 2.schema
+module.exports = mongoose.model('Subscriber', subscriberSchema)
